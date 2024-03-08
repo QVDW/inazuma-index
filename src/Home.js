8017c00c205f8644a7498e3caa322de56cc265bd
Quinten van de Weg <37804654+QVDW@users.noreply.github.com>
2024-03-08 20:34:33 +0100

    Added Player Page
    Added player page + full search bar functionality in homepage and player page

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,40 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import playersData from './players.json';
 import newsData from './news.json';
 import * as imgExports from './img/Exports';
 
-
 const Home = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredPlayers, setFilteredPlayers] = useState(playersData);
+
+    useEffect(() => {
+        setFilteredPlayers(
+            playersData.filter(player =>
+                player.name.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+        );
+    }, [searchTerm]);
+
     return (
         <div class="container">
             <div id="homepage_bg"></div>
             <div id="homepage_content">
                 <div id="homepage">
                     <div id="home_players">
-                        <div id="home_searchbar">
-                            <input type="text" placeholder="Search for a character" />
-                            <button type="submit"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#fefefe" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
-                        </div>
-                        <div id="home_players_list">
-                                {playersData.slice(0, 12).map((player) => (
+                    <div id="home_searchbar">
+                    <input
+                        type="text"
+                        placeholder="Search for a character"
+                        onChange={event => setSearchTerm(event.target.value)}
+                    />
+                    <button type="submit">
+                    <button type="submit"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#fefefe" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+                    </button>
+                </div>
+                        <div id="players_list">
+                            <div id="home_players_list">
+                                {filteredPlayers.slice(0, 12).map((player) => (
                                     <div class="mini_player">
                                         <img src={imgExports[player.img]} alt="Player Image" id="playerimg"/>
                                         <img src={imgExports[player.element]} alt="Background Card" id="mini_player_card"/>
@@ -25,6 +43,7 @@ const Home = () => {
                                         <h3 id="cardName">{player.name}</h3>
                                     </div>
                                 ))}
+                            </div>
                         </div>
                     </div>
                     <h2 id="news_titel">News</h2>
--- src/Players.js
+++ src/Players.js
@@ -1,9 +1,50 @@
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-
+import playersData from './players.json';
+import * as imgExports from './img/Exports';
 
 const Players = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredPlayers, setFilteredPlayers] = useState(playersData);
+
+    useEffect(() => {
+        setFilteredPlayers(
+            playersData.filter(player =>
+                player.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                player.img.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                player.club.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                player.position.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                player.element.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+        );
+    }, [searchTerm]);
+
     return (
-        <div id="noPage">
+        <div class="container">
+            <div id="homepage_bg"></div>
+            <div id="home_players">
+                <div id="home_searchbar">
+                    <input
+                        type="text"
+                        placeholder="Search for tags: name, position, club or element"
+                        onChange={event => setSearchTerm(event.target.value)}
+                    />
+                    <button type="submit">
+                    <button type="submit"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M15.7955 15.8111L21 21M18 10.5C18 14.6421 14.6421 18 10.5 18C6.35786 18 3 14.6421 3 10.5C3 6.35786 6.35786 3 10.5 3C14.6421 3 18 6.35786 18 10.5Z" stroke="#fefefe" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg></button>
+                    </button>
+                </div>
+                <div id="players_list">
+                    {filteredPlayers.map((player) => (
+                        <div class="mini_player">
+                            <img src={imgExports[player.img]} alt="Player Image" id="playerimg"/>
+                            <img src={imgExports[player.element]} alt="Background Card" id="mini_player_card"/>
+                            <img src={imgExports[player.club]} alt="Club Logo" id="mini_player_club"/>
+                            <p>{player.position}</p>
+                            <h3 id="cardName">{player.name}</h3>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
