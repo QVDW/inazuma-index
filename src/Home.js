924bc6b5b397e9e42182f3d1324814cf90a7225a
Quinten van de Weg <37804654+QVDW@users.noreply.github.com>
2024-03-17 02:22:42 +0100

    Homepage redesign
    Made the first step of the homepage redesign

--- src/Home.js
+++ src/Home.js
@@ -1,62 +1,28 @@
-import { useState, useEffect } from 'react';
-import playersData from './players.json';
-import newsData from './news.json';
-import * as imgExports from './img/Exports';
+import { BrowserRouter, Routes, Route, Link, NavLink } from 'react-router-dom';
 
-const Home = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [filteredPlayers, setFilteredPlayers] = useState(playersData);
 
-    useEffect(() => {
-        setFilteredPlayers(
-            playersData.filter(player =>
-                player.name.toLowerCase().includes(searchTerm.toLowerCase())
-            )
-        );
-    }, [searchTerm]);
+
+const Homepage = () => {
 
     return (
-        <div class="container">
-            <div id="background"></div>
-            <div id="homepage_content">
-                <div id="homepage">
-                    <div id="players">
-                        <div id="searchbar">
-                        <input
-                            type="text"
-                            placeholder="Search for a character"
-                            onChange={event => setSearchTerm(event.target.value)}
-                        />
-                        </div>
-                        <div id="players_list">
-                            <div id="home_players_list">
-                                {filteredPlayers.slice(0, 15).filter(player => player.img !== "invis").map((player) => (
-                                    <div class="mini_player">
-                                        <img src={imgExports[player.img]} alt="Player Image" id="playerimg"/>
-                                        <img src={imgExports[player.element]} alt="Background Card" id="mini_player_card"/>
-                                        <img src={imgExports[player.club]} alt="Club Logo" id="mini_player_club"/>
-                                        <img src={imgExports[player.elementIcon]} alt="Element Icon" id="element_icon"/>
-                                        <p>{player.position}</p>
-                                        <h3 id="cardName">{player.name}</h3>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
+        <div id="home_container">
+            <div id="home_fade">
+                    <div id="extra_nav">
+                        <ul>
+                            <li><NavLink to="/Players">Players</NavLink></li>
+                            <li><NavLink to="/Moves">Moves</NavLink></li>
+                            <li><NavLink to="/Builder">Team Builder</NavLink></li>
+                            <li><NavLink to="/News">News</NavLink></li>
+                        </ul>
                     </div>
-                    <h2 id="news_titel">News</h2>
-                    <div id="home_news">
-                        {newsData.slice(0, 3).map((news) => (
-                            <div class="news">
-                                <img src={imgExports[news.thumbnail]} alt="News Image" />
-                                <h3>{news.title}</h3>
-                                <a href={news.link} target="_blank">Click to read more</a>
-                            </div>
-                        ))}
-                    </div>
+                <div id="home_text">
+                    <h1>Inazuma Index</h1>
+                    <p>Explore player stats, move details, build your dream team, and stay updated with the latest news on Victory Road all at Inazuma Index! Your go-to hub for everything Victory Road has to offer!</p>
+                    <NavLink to="/News" id="news_button">News</NavLink>
                 </div>
             </div>
         </div>
     );
 }
 
-export default Home;
+export default Homepage;
